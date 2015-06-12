5c4e088be1936ff256f82040a0115193206336d9
Eric Newton <ericnewton76@gmail.com>
2015-06-12 09:37:45 -0400

    updated build-nuget-package

--- build-nuget-package.cmd
+++ build-nuget-package.cmd
@@ -3,15 +3,15 @@
 setlocal
 set NUGETEXE=%~dp0\packages\NuGet.CommandLine.2.8.5\tools\NuGet.exe
 
-if exist "Build\MvcInterop\." rmdir /s /q Build\MvcInterop 2>NUL
-mkdir Build\MvcInterop 2>NUL
+if exist "Build\Nuget\." rmdir /s /q Build\Nuget 2>NUL
+mkdir Build\Nuget 2>NUL
 
-xcopy /I src\MvcInterop\bin\Release\Mvc* Build\MvcInterop\lib\net40
+xcopy /I src\MvcInterop\bin\Release\Mvc* Build\Nuget\lib\net40
 
-copy MvcInterop.nuspec Build\MvcInterop
+copy MvcInterop.nuspec Build\Nuget
 
-pushd Build\MvcInterop
-"%NUGETEXE%" pack MvcInterop.nuspec -version %APPVEYOR_BUILD_VERSION% -OutputDirectory ..\..
+pushd Build\Nuget
+"%NUGETEXE%" pack MvcInterop.nuspec -version %APPVEYOR_BUILD_VERSION% 
 
 REM appveyor will deploy *.nupkg
 REM "%NUGETEXE%" push MvcInterop.%BUILD_VERSION%.nupkg
