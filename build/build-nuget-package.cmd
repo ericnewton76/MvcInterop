9ec68f31d695d7c5e644c161a8332f744707ee69
Eric Newton <ericnewton76@gmail.com>
2017-12-27 19:20:10 -0500

    moved build scripts into Build

--- build/build-nuget-package.cmd
+++ build/build-nuget-package.cmd
@@ -10,18 +10,18 @@ set PROJECTSRC=MvcInterop
 set NUGETEXE=%~dp0packages\NuGet.CommandLine.2.8.5\tools\NuGet.exe
 if not exist "%NUGETEXE%" echo --Warning, nuget.exe not found in packages. Using C:\bin\nuget.exe & set NUGETEXE=C:\bin\NuGet.exe
 
-echo *Cleaning Nuget\Build directory
-if exist "Build\Nuget\." rmdir /s /q Build\Nuget 2>NUL
-mkdir Build\Nuget 2>NUL
+echo *Cleaning Release\Build directory
+if exist "Release\Nuget\." rmdir /s /q Release\Nuget 2>NUL
+mkdir Release\Nuget 2>NUL
 
-echo *Copying Release build into Build\Nuget.
-xcopy /I src\%PROJECTSRC%\bin\Release\Mvc* Build\Nuget\lib\net40 1>NUL
+echo *Copying Release build into Release\Nuget.
+xcopy /I src\%PROJECTSRC%\bin\Release\Mvc* Release\Nuget\lib\net40 1>NUL
 REM tree /f Build\Nuget
 REM echo.
 
-echo *Copying nuspec into Build\Nuget.
-copy %NUSPECNAME%.nuspec Build\Nuget
-if errorlevel 1 echo failed to copy %NUSPECNAME%.nuspec into Build\Nuget & goto :ERROR
+echo *Copying nuspec into Release\Nuget.
+copy %NUSPECNAME%.nuspec Release\Nuget
+if errorlevel 1 echo failed to copy %NUSPECNAME%.nuspec into Release\Nuget & goto :ERROR
 
 pushd Build\Nuget
 echo *Nuget pack %NUSPECNAME%.nuspec
