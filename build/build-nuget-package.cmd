d822fe76a6988d254b37590cde54dfa4d1f7a9a0
Eric Newton <ericnewton76@gmail.com>
2017-12-27 19:28:04 -0500

    updated build scripts to host build from Release folder

--- build/build-nuget-package.cmd
+++ build/build-nuget-package.cmd
@@ -10,24 +10,30 @@ set PROJECTSRC=MvcInterop
 set NUGETEXE=%~dp0packages\NuGet.CommandLine.2.8.5\tools\NuGet.exe
 if not exist "%NUGETEXE%" echo --Warning, nuget.exe not found in packages. Using C:\bin\nuget.exe & set NUGETEXE=C:\bin\NuGet.exe
 
+pushd ..
+
+echo.
 echo *Cleaning Release\Build directory
 if exist "Release\Nuget\." rmdir /s /q Release\Nuget 2>NUL
 mkdir Release\Nuget 2>NUL
 
+echo.
 echo *Copying Release build into Release\Nuget.
-xcopy /I src\%PROJECTSRC%\bin\Release\Mvc* Release\Nuget\lib\net40 1>NUL
-REM tree /f Build\Nuget
+robocopy /mir src\%PROJECTSRC%\bin\Release\Mvc* Release\Nuget\lib\net40 1>NUL
+REM tree /f Release\Nuget
 REM echo.
 
+echo.
 echo *Copying nuspec into Release\Nuget.
 copy %NUSPECNAME%.nuspec Release\Nuget
 if errorlevel 1 echo failed to copy %NUSPECNAME%.nuspec into Release\Nuget & goto :ERROR
 
-pushd Build\Nuget
+echo.
+pushd Release\Nuget
 echo *Nuget pack %NUSPECNAME%.nuspec
 echo "%NUGETEXE%" pack %NUSPECNAME%.nuspec -version %BUILD_VERSION% 
 "%NUGETEXE%" pack %NUSPECNAME%.nuspec -version %BUILD_VERSION% 
-if errorlevel 1 echo --Error: nuget pack command failed.
+if errorlevel 1 popd&echo --Error: nuget pack command failed.
 REM echo.
 
 REM appveyor will deploy *.nupkg
